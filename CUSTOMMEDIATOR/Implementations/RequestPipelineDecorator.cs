77d21b1b6254effd3d21e60368a9d75cee190ba6
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2026-06-07 03:30:30 +0300

    commit

--- CUSTOMMEDIATOR/Implementations/RequestPipelineDecorator.cs
+++ CUSTOMMEDIATOR/Implementations/RequestPipelineDecorator.cs
@@ -1,5 +1,4 @@
-﻿using CUSTOMMEDIATOR.Helpers;
-using CUSTOMMEDIATOR.Interfaces;
+﻿using CUSTOMMEDIATOR.Interfaces;
 using FluentValidation;
 using FluentValidation.Results;
 
@@ -13,9 +12,6 @@ public class RequestPipelineDecorator<TRequest, TResponse>(
 ) : IRequestHandler<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
-    static readonly Type _requestType = typeof(TRequest);
-    static readonly Type _processorType = typeof(IRequestPostProcessor<TRequest, TResponse>);
-
     readonly IValidator<TRequest>[] _validators = [.. validators];
     readonly IRequestPreProcessor<TRequest>[] _preProcessors = [.. preProcessors];
     readonly IRequestPostProcessor<TRequest, TResponse>[] _postProcessors = [.. postProcessors];
@@ -25,17 +21,18 @@ public class RequestPipelineDecorator<TRequest, TResponse>(
         CancellationToken cancellationToken = default
     )
     {
-        for (var i = 0; i < _preProcessors.Length; i++)
+        for (int i = 0; i < _preProcessors.Length; i++)
             await _preProcessors[i].Process(request, cancellationToken);
 
         if (_validators.Length > 0)
         {
-            var context = new ValidationContext<TRequest>(request);
-            var failures = new List<ValidationFailure>(_validators.Length);
+            ValidationContext<TRequest> context = new(request);
+            List<ValidationFailure> failures = new(_validators.Length);
 
-            for (var i = 0; i < _validators.Length; i++)
+            for (int i = 0; i < _validators.Length; i++)
             {
-                var result = await _validators[i].ValidateAsync(context, cancellationToken);
+                ValidationResult result = await _validators[i]
+                    .ValidateAsync(context, cancellationToken);
                 if (!result.IsValid)
                     failures.AddRange(result.Errors);
             }
@@ -46,16 +43,8 @@ public class RequestPipelineDecorator<TRequest, TResponse>(
 
         var response = await innerHandler.Handle(request, cancellationToken);
 
-        if (_postProcessors.Length > 0)
-        {
-            var invoker = PostProcessorInvokerCache<TResponse>.GetOrAdd(
-                _requestType,
-                _processorType
-            );
-
-            for (var i = 0; i < _postProcessors.Length; i++)
-                await invoker(_postProcessors[i], request, response, cancellationToken);
-        }
+        for (int i = 0; i < _postProcessors.Length; i++)
+            await _postProcessors[i].Process(request, response, cancellationToken);
 
         return response;
     }
