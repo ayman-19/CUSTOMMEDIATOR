9b86ae3c1220ea9e307dd6b42d7e2248211090cd
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2026-06-10 00:39:28 +0300

    commit

---
+++ CUSTOMMEDIATOR/Interfaces/IPipelineBehavior.cs
@@ -0,0 +1,11 @@
+﻿namespace CUSTOMMEDIATOR.Interfaces;
+
+public interface IPipelineBehavior<TRequest, TResponse>
+	where TRequest : notnull
+{
+	Task<TResponse> Handle(
+		TRequest request,
+		RequestHandlerDelegate<TResponse> next,
+		CancellationToken cancellationToken
+	);
+}
